238721743785b1654cafcf290ed1eba4d80ab066
gdsr-abhishek <gdsr.abhishek@gmail.com>
2024-01-07 00:15:17 +0530

    Adding State and API call

--- app/src/App.js
+++ app/src/App.js
@@ -1,34 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from 'react';
 
-
-function App() {
-
+ let App= ()=>{
+   const [rows,setRows] = useState([{}]);
+   useEffect(()=>{async function getData(){
+   try{
+    const res=  await fetch("http://localhost:8000/ps/all")
+    const result = await res.json();
+    setRows(result);
+  } catch (error) {
+    console.error('Error fetching data:', error.message);
+  }
+  }
+  getData();
+  },[rows]);
   return (
     <div className="App">
     
       <h1 className="appTitle">Process Monitor</h1>
-
-      <div className="processArea">
       <div className="searchArea">
         searchArea
       </div>
+      <div className="processArea">
       <div >
         <table className="tableArea">
-          <th className="tableRow">
-           <td>USER</td>
-           <td>Process ID</td>
-           <td>RAM Usage %</td>
-           <td>Memory Usage %</td>        
-           <td>VSZ</td>        
-           <td>RSS</td>
-           <td>TTY</td>
-           <td>STAT</td>   
-           <td>START</td>   
-           <td>TIME</td>     
-           <td>COMMAND</td>          
-                       
-          </th>
+         <thead>
+          <tr className="tableRow">
+           <th>USER</th>
+           <th>Process ID</th>
+           <th>RAM Usage %</th>
+           <th>Memory Usage %</th>        
+           <th>VSZ</th>        
+           <th>RSS</th>
+           <th>TTY</th>
+           <th>STAT</th>   
+           <th>START</th>   
+           <th>TIME</th>     
+           <th>COMMAND</th>                         
+          </tr>
+          </thead>
+          <tbody>
+        {rows.map(row=><RowComponent key={row.PID} row ={row}/>)}
+        </tbody>
         </table>
       </div>
 
@@ -37,5 +50,36 @@ function App() {
     </div>
   );
 }
-
+/*
+ {
+        "USER": "root",
+        "PID": "1",
+        "%CPU": "0.0",
+        "%MEM": "0.0",
+        "VSZ": "167028",
+        "RSS": "12100",
+        "TTY": "?",
+        "STAT": "Ss",
+        "START": "22:10",
+        "TIME": "0:01",
+        "COMMAND": "/sbin/init"
+    }
+*/
+function RowComponent({row}){
+return(
+  <tr>
+    <td>{row.USER}</td>
+    <td>{row.PID}</td>
+    <td>{row['%CPU']}</td>
+    <td>{row['%MEM']}</td>
+    <td>{row.VSZ}</td>
+    <td>{row.RSS}</td>
+    <td>{row.TTY}</td>
+    <td>{row.STAT}</td>
+    <td>{row.START}</td>
+    <td>{row.TIME}</td>
+    <td>{row.COMMAND}</td>
+  </tr>
+);
+}
 export default App;
